f447ca21cf9080c62237b992cbe95195ed8b6ecf
Wooram Moon <wmoon0024@gmail.com>
2023-06-21 00:20:28 +0900

    로그인회원가입 완료하였습니다

--- src/main/java/com/jpa/yeongaesomun/controller/LoginPageController.java
+++ src/main/java/com/jpa/yeongaesomun/controller/LoginPageController.java
@@ -1,4 +1,22 @@
 package com.jpa.yeongaesomun.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/main-page/*")
 public class LoginPageController {
+    @GetMapping("login")
+    public void login(){
+
+    }
+
+    @GetMapping("register")
+    public void register(){
+
+    }
 }
