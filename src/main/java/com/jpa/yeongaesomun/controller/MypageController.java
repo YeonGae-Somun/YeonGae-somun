4d53342e19e243a08ba4802ee62acef76aa8f492
Wooram Moon <wmoon0024@gmail.com>
2023-06-30 18:35:31 +0900

    마이페이지 업데이트완료

---
+++ src/main/java/com/jpa/yeongaesomun/controller/MypageController.java
@@ -0,0 +1,16 @@
+package com.jpa.yeongaesomun.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/mypage/*")
+public class MypageController {
+    @GetMapping("mypage-supporter")
+    public void goToMypageSupporter(){
+
+    }
+}
