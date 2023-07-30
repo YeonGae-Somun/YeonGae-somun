b285b8e6b922c6f81cabc40085f65fe7a45dc925
DongJae92 <dongjae2379@gmail.com>
2023-07-30 10:45:02 +0900

    productExperience entity

---
+++ src/main/java/com/jpa/yeongaesomun/entity/ProductExperienceFile.java
@@ -0,0 +1,27 @@
+package com.jpa.yeongaesomun.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TBL_PRODUCT_EXPERIENCE_FILE")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductExperienceFile {
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ProductExperience productExperience;
+
+    @Builder
+
+    public ProductExperienceFile(ProductExperience productExperience) {
+        this.productExperience = productExperience;
+    }
+}
--- src/main/java/com/jpa/yeongaesomun/entity/User.java
+++ src/main/java/com/jpa/yeongaesomun/entity/User.java
@@ -1,5 +1,20 @@
 package com.jpa.yeongaesomun.entity;
 
-public class User {
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_USER")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
 }
