d2e43c2fb0c489c0591f5f750b09d2df5075bf99
Jaehee-code <jaeheej93@gmail.com>
2023-07-30 10:53:37 +0900

    스타트업 엔티티 추가

--- src/main/java/com/jpa/yeongaesomun/entity/member/StartUp.java
+++ src/main/java/com/jpa/yeongaesomun/entity/member/StartUp.java
@@ -1,5 +1,26 @@
-package com.jpa.yeongaesomun.entity;
+package com.jpa.yeongaesomun.entity.member;
 
-public class StartUp {
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Getter @Setter
+@ToString
+@Table(name = "TBL_STARTUP")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@PrimaryKeyJoinColumn
+public class StartUp extends User{
+
+    private String sBusinessNumber;
+    private String sIntroduction;
+    private String sService;
+    private String sManager;
+    private String sEmployNumber;
+    private String sAddress;
+    private String sEmail;
+    private String sHomepage;
+    private String sCreationDate;
 }
--- src/main/java/com/jpa/yeongaesomun/entity/member/User.java
+++ src/main/java/com/jpa/yeongaesomun/entity/member/User.java
@@ -1,11 +1,29 @@
-package com.jpa.yeongaesomun.entity;
+package com.jpa.yeongaesomun.entity.member;
 
+import com.jpa.yeongaesomun.audit.Period;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-@Getter
-public class User {
+@Table(name = "TBL_USER")
+@Getter @Setter
+@ToString
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+public class User extends Period {
+
+    @Id
+    @GeneratedValue
+    private long Id;
+    private String UserNickname;
+    private String UserPassword;
+    private String UserLoginType;
+    private String UserEmail;
+    private String UserType;
+    private String UserRole;
+    private String UserStatus;
 
 }
